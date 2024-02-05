995bc38ddc9b21523ba0555a633265327ed02b84
denis <denisskholmov@gmail.com>
2024-02-05 22:22:38 +0500

    дописал evolution tech, теперь буду делать адаптацию

--- js/index.js
+++ js/index.js
@@ -1,4 +1,44 @@
 "use strict";
+const loaderFront = document.querySelector(".loader__front");
+const loaderBack = document.querySelector(".loader__back");
+
+
+const loaderBtn1 = document.querySelector(".loader__btn-item_1");
+const loaderBtn2 = document.querySelector(".loader__btn-item_2");
+const loaderBtn3 = document.querySelector(".loader__btn-item_3");
+const loaderBtn4 = document.querySelector(".loader__btn-item_4");
+
+const loaderInf = document.querySelector(".loader__inf");
+
+loaderBtn1.addEventListener("mouseenter", ()=>{
+	loaderFront.innerHTML = "<img src='./images/loader-front-top.png' alt=''>";
+})
+
+loaderBtn1.addEventListener("mouseout", ()=>{
+	loaderFront.innerHTML = "<img src='./images/loader-front.png' alt=''>";
+})
+
+
+
+loaderBtn3.addEventListener("mouseenter", ()=>{
+	loaderBack.innerHTML = "<img src='./images/loader-back-top.png' alt=''>";
+	loaderInf.classList.add("active");
+})
+
+loaderBtn3.addEventListener("mouseout", ()=>{
+	loaderBack.innerHTML = "<img src='./images/loader-back.png' alt=''>";
+	loaderInf.classList.remove("active");
+})
+
+loaderBtn4.addEventListener("mouseenter", ()=>{
+	loaderBack.innerHTML = "<img src='./images/loader-back-bottom.png' alt=''>";
+})
+
+loaderBtn4.addEventListener("mouseout", ()=>{
+	loaderBack.innerHTML = "<img src='./images/loader-back.png' alt=''>";
+})
+
+
 
 
 
@@ -17,3 +57,5 @@ function addStyleOnResize(){
 }
 
 window.addEventListener("resize", addStyleOnResize);
+
+
